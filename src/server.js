c546499dc2658cc8180be86ee2a506a1b69c9cd2
VictorMeneghini <vhmeneghini@gmail.com>
2019-08-06 21:39:20 -0300

    Create mongoose conect and MVC Schema for Dev,Like and Dislike

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.send('hi');
-});
+mongoose.connect(
+  '',
+  {useNewUrlParser: true}
+)
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333);
